8eb82a2c65a3f8ad38dfb37f4d5deb5d69829a71
雨宫奈 <hello@aquanir.cc>
2022-05-13 23:47:08 +0800

    update

---
+++ components/Footer.tsx
@@ -0,0 +1,12 @@
+const Footer = () => {
+  return (
+    <footer className="text-xs text-center p-6 primary-text">
+      <div>
+        Sakana &copy; {new Date().getFullYear()}
+      </div>
+      <div>Powered by Notion, Next.js & Vercel.</div>
+    </footer>
+  )
+}
+
+export default Footer
